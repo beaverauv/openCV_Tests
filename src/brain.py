c0c70d15abc66a3ae7979162216eda8305c48118
reisnermax <maximusreisner@gmail.com>
2019-03-15 20:14:22 -0400

    got rid of unusable names

--- src/brain.py
+++ src/brain.py
@@ -1,6 +1,11 @@
 import gate_detector.number_poles as numpoles
+import gate_detector.two_pole_detect as twopole
 import cv2
+import time
 
 img = cv2.imread('../photos/1.jpg', 1)
 
-print(numpoles.execute(img))
+start = time.time()
+print(twopole.execute(img))
+end = time.time()
+print("You could use around " + str(int(1/(end-start))) + " FPS")
